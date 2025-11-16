f38f8b189c9dfde46ed07d37d02848c499e61ff9
Nelson Kouame <kouamenelson47@gmail.com>
2025-11-16 03:16:58 +0100

    change time

--- src/scheduler/daily-report.cron.ts
+++ src/scheduler/daily-report.cron.ts
@@ -22,7 +22,8 @@ import {
 export class StartDailyReportJob implements OnModuleInit, OnModuleDestroy {
   private readonly logger = new Logger(StartDailyReportJob.name);
   private readonly cronJobs: Map<string, any> = new Map();
-  private readonly CRON_SCHEDULE = '0 19 * * *';
+  // private readonly CRON_SCHEDULE = '0 19 * * *';
+  private readonly CRON_SCHEDULE = '30 2 * * *';
 
   constructor(
     @Inject(DashbordRepositoryName)
@@ -99,7 +100,7 @@ export class StartDailyReportJob implements OnModuleInit, OnModuleDestroy {
       async () => {
         await this.executeDailyReport(tenantId);
       },
-      { timezone: 'Europe/Paris' },
+      { timezone: 'Africa/Abidjan' },
     );
 
     this.cronJobs.set(tenantId, {
